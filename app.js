9f5ffac1a0952e65607e875f81fc9b77369044de
iamphillymusic <cm.theophilus@gmail.com>
2022-08-09 18:14:05 +0100

    es6 constructors

--- app.js
+++ app.js
@@ -1,35 +1,63 @@
-// // Person constructor
-// function Person(name, dob) {
+// // // Person constructor
+// // function Person(name, dob) {
+// //   this.name = name;
+// //   // this.age = age;
+// //   this.birthday = new Date(dob);
+// //   this.calculateAge = function(){
+// //     const diff =  Date.now() - this.birthday.getTime();
+// //     const ageDate = new Date(diff);
+// //     return Math.abs(ageDate.getUTCFullYear() - 1970);
+// //   }
+// // }
+
+// // // const brad = new Person('Brad', 36);
+// // // const john = new Person('John', 30);
+
+// // // console.log(john.age);
+
+// // const brad = new Person('Brad', '9-10-1981');
+// // console.log(brad.calculateAge());
+
+// function Person(name, dob){
 //   this.name = name;
 //   // this.age = age;
 //   this.birthday = new Date(dob);
 //   this.calculateAge = function(){
-//     const diff =  Date.now() - this.birthday.getTime();
+//     const diff = Date.now() - this.birthday.getTime();
 //     const ageDate = new Date(diff);
-//     return Math.abs(ageDate.getUTCFullYear() - 1970);
+//     return  Math.abs(ageDate.getUTCFullYear() - 1970);
 //   }
 // }
+// // const brad = new Person('Brad', 45);
+// // const john = new Person('John', 45);
+// // const doe = new Person('Doe', 45);
+// const Doe = new Person('doe', '9-10-1981')
+// console.log(Doe.calculateAge());
 
-// // const brad = new Person('Brad', 36);
-// // const john = new Person('John', 30);
 
-// // console.log(john.age);
+// const name1 = 'jeff';
+// const name2 = new String('jeff');
 
-// const brad = new Person('Brad', '9-10-1981');
-// console.log(brad.calculateAge());
+// // console.log(name1);
 
-function Person(name, dob){
-  this.name = name;
-  // this.age = age;
-  this.birthday = new Date(dob);
-  this.calculateAge = function(){
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return  Math.abs(ageDate.getUTCFullYear() - 1970);
-  }
-}
-// const brad = new Person('Brad', 45);
-// const john = new Person('John', 45);
-// const doe = new Person('Doe', 45);
-const Doe = new Person('doe', '9-10-1981')
-console.log(Doe.calculateAge());
+
+// const numb1 = 5;
+// const numb2 = new Number(5);
+// // console.log(numb2)
+
+// const bool1 = true;
+// const bool2 = new Boolean(true);
+// // console.log(bool2);
+
+// const getSum1 = function(x, y){
+//   return x + y;
+// }
+
+// const john1 = {name: 'john'};
+// const john2 = new Object({name: "john"})
+// // console.log(john2);
+
+// const arr1 = [1,2,3,4]
+// const arr2 = new Array(1,2,3,4);
+// console.log(arr1[2])
+
